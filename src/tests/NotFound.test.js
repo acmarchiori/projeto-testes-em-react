6049e61fb720f3bf61c9bc566fb24df8ebe49c99
acmarchiori <ac_marchiori@yahoo.com.br>
2023-02-27 19:02:19 -0300

    Finalizado requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found;', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(title).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem;', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image).toHaveAttribute('alt', 'Pikachu crying because the page requested was not found');
+  });
+});
