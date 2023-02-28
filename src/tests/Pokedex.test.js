6d14363e27b32f29791ba595cf90a91f4ffa09a2
acmarchiori <ac_marchiori@yahoo.com.br>
2023-02-27 23:26:11 -0300

    Finalizado requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,166 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Pokedex } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const isPokemonFavoriteById = {
+  4: false,
+  10: false,
+  23: false,
+  25: false,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+const pokemonList = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://archives.bulbagarden.net/media/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://archives.bulbagarden.net/media/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://archives.bulbagarden.net/media/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://archives.bulbagarden.net/media/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://archives.bulbagarden.net/media/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://archives.bulbagarden.net/media/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+  },
+  {
+    id: 10,
+    name: 'Caterpie',
+    type: 'Bug',
+    averageWeight: {
+      value: '2.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://archives.bulbagarden.net/media/upload/8/83/Spr_5b_010.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Johto Route 30',
+        map: 'https://archives.bulbagarden.net/media/upload/7/76/Johto_Route_30_Map.png',
+      },
+      {
+        location: 'Johto Route 31',
+        map: 'https://archives.bulbagarden.net/media/upload/2/2b/Johto_Route_31_Map.png',
+      },
+      {
+        location: 'Ilex Forest',
+        map: 'https://archives.bulbagarden.net/media/upload/a/ae/Johto_Ilex_Forest_Map.png',
+      },
+      {
+        location: 'Johto National Park',
+        map: 'https://archives.bulbagarden.net/media/upload/4/4e/Johto_National_Park_Map.png',
+      },
+    ],
+    summary: 'For protection, it releases a horrible stench from the antennae on its head to drive away enemies.',
+  },
+];
+
+const btnText = 'Próximo Pokémon';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon;', () => {
+    renderWithRouter(<App />);
+
+    const title = screen.getByRole('heading', { level: 2, name: 'Encountered Pokémon' });
+    expect(title).toBeInTheDocument();
+  });
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
+    renderWithRouter(<Pokedex
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+      pokemonList={ pokemonList }
+    />);
+
+    const btn = screen.getByText(btnText);
+    expect(btn).toBeInTheDocument();
+
+    const nextPokemon = screen.getByRole('button', { name: btnText });
+    userEvent.click(nextPokemon);
+    const pokemon2 = screen.getByText('Charmander');
+    expect(pokemon2).toBeInTheDocument();
+
+    userEvent.click(nextPokemon);
+    const pokemon3 = screen.getByText('Caterpie');
+    expect(pokemon3).toBeInTheDocument();
+
+    userEvent.click(nextPokemon);
+    const pokemon1 = screen.getByText('Pikachu');
+    expect(pokemon1).toBeInTheDocument();
+  });
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    renderWithRouter(<App />);
+
+    const nextPokemon = screen.getByRole('button', { name: btnText });
+    userEvent.click(nextPokemon);
+    const pokemon2 = screen.getByText('Charmander');
+    expect(pokemon2).toBeInTheDocument();
+    expect(screen.queryByText('Caterpie')).not.toBeInTheDocument();
+    expect(screen.queryByText('Pikachu')).not.toBeInTheDocument();
+  });
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+
+    const btn = screen.getAllByTestId('pokemon-type-button');
+    expect(btn).toHaveLength(7);
+  });
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+
+    const all = screen.getByRole('button', { name: 'All' });
+    expect(all).toBeInTheDocument();
+
+    const bug = screen.getByRole('button', { name: 'Bug' });
+    userEvent.click(bug);
+    const bugPokemon = screen.getByText('Caterpie');
+    expect(bugPokemon).toBeInTheDocument();
+    userEvent.click(all);
+
+    const pokemon1 = screen.getByText('Pikachu');
+    expect(pokemon1).toBeInTheDocument();
+  });
+});
